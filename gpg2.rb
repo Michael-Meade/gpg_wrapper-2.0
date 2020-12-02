4d52761e6073daee319fa91dc92e9d0fdcf66117
Michael-Meade <mameade@utica.edu>
2020-12-01 21:21:47 -0500

    Source updated at 2020-12-01 21:21:47 -0500.

---
+++ gpg2.rb
@@ -0,0 +1,47 @@
+require 'open3'
+class CMD
+	def run(cmd)
+		stdout, status = Open3.capture2(cmd)
+	end
+end
+class Import < CMD
+	def import_key(file)
+		run("gpg --import #{file}")
+	end
+end
+class Encrypt < CMD
+	def encrypt_msg(text_to_encrypt, pubkey_name)
+		run("echo '#{text_to_encrypt}' | gpg  --encrypt -a -r '#{pubkey_name}' --always-trust")
+	end
+	def encrypt_file(file_to_encrypt, pubkey_name)
+		run("echo '#{text_to_encrypt}' | gpg  --encrypt -a -r '#{pubkey_name}' --always-trust")
+	end
+end
+class Sign < CMD
+	# note: its not a good idea to hard code your pass. 
+	def sign_msg(pass, pubkey_name, file)
+		run("echo '#{pass}' | gpg --batch --yes --passphrase-fd 0 --local-user #{pubkey_name} --sign #{file}")
+	end
+end
+
+class Export < CMD
+	def export_public(pubkey_name, file_name = nil)
+		if file_name.nil?
+			run("gpg --armor --export #{pubkey_name} > #{filename}")
+		else
+			run("gpg --armor --export #{pubkey_name}")
+		end
+	end
+end
+
+
+
+puts
+# Import key
+#Import.new.import_key("t.txt")
+
+# Encrypt text
+#puts Encrypt.new.encrypt_msg("Hi", "BirdFart") 
+
+# Export Public key
+#puts Export.new.export_public("BirdFart")
