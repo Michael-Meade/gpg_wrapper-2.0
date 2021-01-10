a83837f1b0372682d5081d6c0a02b055df96d5d9
Michael-Meade <mameade@utica.edu>
2021-01-10 17:57:22 -0500

    made into a gem

--- main.rb
+++ main.rb
@@ -1,12 +1,6 @@
 require 'gpg'
 
-#Commands.export_private("mameade@utica.edu", output=nil)
-#Key.list_keys
-#Commands.sign_file("mameade@utica.edu","order.php")
-#Commands.verfiy("order.php.gpg")
-#Commands.sign_file("mameade@utica.edu", "order.php")
-#encrypt_file("mameade@utica.edu", "order.php")
 Commands.encrypt_file("BirdFart", "main.rb")
 
-#Commands.sign_encrypt("mameade@utica.edu", "mameade@utica.edu", "derby3333", "order.php")
+
 #Keys.import("import.gpg")
